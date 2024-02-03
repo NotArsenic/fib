a97a234d9287820c1075f1f878e94f7540b6f6ee
Arsenic <cryoarsenic@gmail.com>
2024-02-03 14:01:40 +0530

    this is 1st commit

--- fib.cpp
+++ fib.cpp
@@ -1,11 +1,16 @@
 #include <cstdio>
 #include <cstdint>
+#include <iostream>
 
-static uint64_t fib(uint64_t n) {
-  if (n <= 1) return n;
+static uint64_t fib(uint64_t n)
+{
+  if (n <= 1)
+    return n;
   return fib(n - 1) + fib(n - 2);
 }
 
-int main() {
+// hello
+int main()
+{
   printf("%llu \n", fib(47));
 }
